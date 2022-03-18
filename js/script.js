91dc09e34f7c3106cb2e84a54611471e773e3c64
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-18 17:22:59 +0100

    fatto v-for per stampare i li nell ul

--- js/script.js
+++ js/script.js
@@ -2,6 +2,31 @@
 const root = new Vue({
     el: "#root",
     data: {
+        todos: [
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quos error, expedita vitae natus doloribus libero quia a et laborum odit culpa ipsam blanditiis nostrum impedit qui laboriosam corrupti iure.",
+                done: false,
+            },
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                done: false,
+            },
+            {
+                text: "Est quos error, expedita vitae natus doloribus libero quia a et laborum odit culpa ipsam blanditiis nostrum impedit qui laboriosam corrupti iure.",
+                done: false,
+            },
+            {
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quos error",
+                done: false,
+            },
+            {
+                text: "Lorem ipsum dolor sit. Est quos error, expedita vitae natus doloribus libero quia a et laborum odit culpa ipsam blanditiis nostrum impedit qui laboriosam corrupti iure.",
+                done: false,
+            },
+        ],
+    },
+
+    method:{
 
     }
 })
