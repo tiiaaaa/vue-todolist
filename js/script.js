c5dc106270b45ff9ef0aa1bf9a22f613e98d4380
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-19 17:35:51 +0100

    implementate funzioni di aggiunta e rimozione todoelement, tramite click del bottone aggiungi e click della x per rimuovere gli elementi

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 const root = new Vue({
     el: "#root",
     data: {
-        todos: [
+
+        todoList: [
             {
                 text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quos error, expedita vitae natus doloribus libero quia a et laborum odit culpa ipsam blanditiis nostrum impedit qui laboriosam corrupti iure.",
                 done: false,
@@ -24,9 +25,30 @@ const root = new Vue({
                 done: false,
             },
         ],
+
+        todoContent: "",
+
     },
 
-    method:{
-        
+    methods:{
+        //!Non capisco perchè questa funzione prende tramite v-model il testo dell'input e aggiunge un todoelement vuoto senza testo all'interno
+        addTodoElement(todoContent){
+            if(todoContent.trim() != ""){
+                this.todoList.push(todoContent.trim());
+                this.todoContent = "";
+            }else{
+                console.log("Errore")
+            }
+        },
+
+        removeTodoElement(index){
+            if(this.todoList[index] != undefined){
+                this.todoList.splice(index, 1);
+                console.log(this.todoList)
+            }else{
+                console.log("Non c'è nessun elemento da rimuovere")
+            }
+        }
+    
     }
 });
